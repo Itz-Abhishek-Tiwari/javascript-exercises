7f1094ec29010c558b3734b5c96fe75cba165f88
Abhishek Tiwari <abhitiwariabhi5@gmail.com>
2024-04-08 22:46:29 +0530

    finish exercise 03 reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let stringArr = [...str];
+  let newArr = [];
+  for (let i = stringArr.length - 1; i >= 0; i--) {
+    newArr.push(stringArr[i]);
+  }
+  return (str = newArr.join(""));
 };
 
 // Do not edit below this line
