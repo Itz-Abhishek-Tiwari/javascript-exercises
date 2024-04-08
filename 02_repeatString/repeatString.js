dad2175f473bde67e5f54b12164a9b105534c08f
Abhishek Tiwari <abhitiwariabhi5@gmail.com>
2024-04-08 22:31:42 +0530

    finsh exercises 02 repeat String

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let str = "";
+  if (num < 0) return "ERROR";
+  for (let i = 0; i < num; i++) {
+    str += string;
+  }
+  return str;
 };
+
+let number;
+let getRandomNum = function () {
+  return (number = parseInt(Math.random() * 10));
+};
+console.log(getRandomNum());
 
 // Do not edit below this line
 module.exports = repeatString;
