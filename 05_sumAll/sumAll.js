3ab4dcf5efa80222bc27b14541a4b4ef8860a104
Abhishek Tiwari <abhitiwariabhi5@gmail.com>
2024-04-09 16:12:52 +0530

    finsih Exercise 05 - sumAll using if else

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function (elementOne, elementTwo) {
+  let sum = 0;
+  if (elementOne > elementTwo) {
+    [elementOne, elementTwo] = [elementTwo, elementOne];
+  }
+  if (elementOne < 0 || elementTwo < 0) return "ERROR";
+  if (typeof elementOne !== "number" || typeof elementTwo !== "number") {
+    return "ERROR";
+  } else {
+    for (let i = elementOne; i <= elementTwo; i++) sum += i;
+  }
 
+  return sum;
 };
-
+console.log(sumAll(1, [1, 2]));
 // Do not edit below this line
 module.exports = sumAll;
