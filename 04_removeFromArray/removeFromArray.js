f30c4655f3f6d0f1d1d71100ef4286e129adca70
Abhishek Tiwari <abhitiwariabhi5@gmail.com>
2024-04-09 10:02:45 +0530

    unable to pass last two case

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function (arrayOfElements, ...elements) {
+  let number;
+  for (let j = 0; j < arguments.length - 1; j++) {
+    number = elements[j];
 
+    console.log(typeof number);
+    for (let i = 0; i < arrayOfElements.length; i++) {
+      if (typeof number !== "number") continue;
+      if (arrayOfElements[i] % number === 0) {
+        arrayOfElements.splice(i, 1);
+      }
+      if (arrayOfElements[i] === number) {
+        arrayOfElements.splice(i, 1);
+      }
+    }
+  }
+  return arrayOfElements;
 };
 
+console.log(removeFromArray([1, 2, 3], "1", 3));
 // Do not edit below this line
 module.exports = removeFromArray;
