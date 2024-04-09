9987fc69f6ac34d93330a714563d00db15ccc296
Abhishek Tiwari <abhitiwariabhi5@gmail.com>
2024-04-09 21:37:09 +0530

    finsih exersice 07 using google to do Math.round()

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let result = ((temp - 32) / 9) * 5;
+  return (result = Math.round(result * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let result = (temp * 9) / 5 + 32;
+  return (result = Math.round(result * 10) / 10);
 };
 
+console.log(convertToCelsius(32));
+console.log(convertToFahrenheit(0));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
