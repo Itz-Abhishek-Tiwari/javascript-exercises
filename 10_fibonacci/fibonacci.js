7d3f19221f77c67a7ef8cd4f0f70eafe29c464dc
Abhishek Tiwari <69257109+Itz-Abhishek-Tiwari@users.noreply.github.com>
2024-04-28 20:23:08 +0530

    finsih fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS";
+  } else {
+    let a = 0;
+    let b = 1;
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+      sum = a + b;
+      b = a;
+      a = sum;
+    }
+    return sum;
+  }
 };
 
+console.log(fibonacci(-4));
 // Do not edit below this line
 module.exports = fibonacci;
